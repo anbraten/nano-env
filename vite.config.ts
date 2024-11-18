892e61e9a8a9e2b562bda285d9465bb28998eb12
Anbraten <anton@ju60.de>
2024-11-18 16:11:27 +0100

    :bug: disable pwa for now

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt'],
-      workbox: {
-        cleanupOutdatedCaches: true,
-      },
-      srcDir: 'src',
-      filename: 'sandbox/service-worker.ts',
-      strategies: 'injectManifest',
-      devOptions: {
-        enabled: true,
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   includeAssets: ['favicon.ico', 'robots.txt'],
+    //   workbox: {
+    //     cleanupOutdatedCaches: true,
+    //   },
+    //   srcDir: 'src',
+    //   filename: 'sandbox/service-worker.mjs',
+    //   strategies: 'injectManifest',
+    //   devOptions: {
+    //     enabled: true,
+    //   },
+    // }),
   ],
   server: {
     headers: {
